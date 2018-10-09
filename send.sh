c075bb14ae4e3cbeef5b7015f481e86c18c35f02
imi47 <imi4747@gmail.com>
2018-10-09 17:25:34 +0500

    added send.sh and a .gitignore for it

---
+++ send.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp resources/views/user/latest-updates.blade.php /home/imran/Documents/send
+cp resources/views/user/manage-profile.blade.php /home/imran/Documents/send
+cp resources/views/user/master_layout.blade.php /home/imran/Documents/send
+cp resources/views/user/master_layout2.blade.php /home/imran/Documents/send
+cp resources/views/user/register_step_1.blade.php /home/imran/Documents/send
+cp resources/views/user/statistics.blade.php /home/imran/Documents/send
+cp resources/views/user/user_master_layout1.blade.php /home/imran/Documents/send
